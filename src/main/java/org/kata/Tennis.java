cbcbae6a128918b0f69f659cf68f6ddeaf12ae3e
Jonathan Faber <jonathan.faber@autentia.com>
2020-09-18 10:50:33 +0200

    base project

---
+++ src/main/java/org/kata/Tennis.java
@@ -0,0 +1,8 @@
+package org.kata;
+
+
+public class Tennis {
+    public static void main(String[] args) {
+        System.out.println("Tennis kata!");
+    }
+}
---
+++ src/test/java/org/kata/TennisTest.java
@@ -0,0 +1,15 @@
+package org.kata;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.is;
+
+
+public class TennisTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
